76db895077ed0284f93a19eabbc9fb8ca5978bfc
moban <18215615808@163.com>
2020-05-16 17:49:34 +0800

    5:49

--- src/views/pay/pay.vue
+++ src/views/pay/pay.vue
@@ -10,7 +10,24 @@
       </div>
     </div>
   </div>
- </div>
+  <div class="header">
+    <div class="h-con box a-center space-between">
+      <div class="h-title">
+        {{month}} 薪酬预算
+      </div>
+      <div class="h-time">
+        <el-date-picker
+            :editable="false"
+            v-model="selectMonth"
+            type="month"
+            :picker-options="pickerOptions"
+            @change="changeMonth"
+            placeholder="请选择月份">
+        </el-date-picker>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -18,18 +35,39 @@
    data () {
      return {
        icon:'',
-       title:''
+       title:'',
+       month:'',
+       selectMonth:'',
+       pickerOptions: {
+          shortcuts: [{
+            text: '本月',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: '上月',
+            onClick(picker) {
+              const start = new Date();
+              start.setMonth(start.getMonth() - 1);
+              picker.$emit('pick', start);
+            }
+          }]
+        },
      }
    },
    components: {
 
    },
    methods: {
-
+     changeMonth(val){
+       this.month = this.$dayjs(val).format('YYYY年MM月')
+       console.log(this.month);
+     }
    },
    mounted() {
      this.icon = this.$route.meta.icon
      this.title = this.$route.meta.enTitle
+     this.selectMonth = this.month = this.$dayjs(new Date()).format('YYYY年MM月')
    },
    watch: {
 
@@ -57,5 +95,29 @@
       }
     }
   }
+  .header {
+  margin-top: 24px;
+  background: #fff;
+  .h-con {
+    .h-title {
+      font-size: 24px;
+      font-weight: 700;
+      padding-left: 15px;
+      height: 80px;
+      line-height: 80px;
+    }
+    .h-time {
+      position: relative;
+      right: 30px;
+    }
+  }
+  .h-desc {
+    background: #E5F2E1;
+    height: 50px;
+    line-height: 50px;
+    padding-left: 15px;
+    font-size: 14px;
+  }
+}
 }
 </style>
